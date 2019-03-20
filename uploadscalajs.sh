4f2d9892d724fb7a1043c9ce175004f707ed18b5
alex <enna@mailbox.org>
2019-03-20 11:41:46 +0100

    Initialize new Javascript

--- uploadscalajs.sh
+++ uploadscalajs.sh
@@ -1,6 +1,7 @@
 TARGET=target/scala-2.12
 CERT=~/Desktop/eternitas/eternitaskeys1.pem
 SCPTARGET=bitnami@34.204.6.252:/home/bitnami/apps/wordpress/htdocs/js/
+WEBAPP=src/main/webapp
 declare -a SCRIPTS=(
     "eternitas-wordpress-opt.js"
     "eternitas-wordpress-opt.js.map"
@@ -12,6 +13,8 @@ sbt fullOptJS
 for script in "${SCRIPTS[@]}" 
 do
 #aws s3 cp $TARGET/$script s3://data.lyrx.de/js/$script
-scp -i $CERT  $TARGET/$script  $SCPTARGET
+#scp -i $CERT  $TARGET/$script  $SCPTARGET
+cp   $TARGET/$script  $WEBAPP/js
 done 
-scp -i $CERT src/main/js/eternitas.js   $SCPTARGET
+#scp -i $CERT src/main/js/eternitas.js   $SCPTARGET
+cp src/main/js/eternitas.js  $WEBAPP/js
