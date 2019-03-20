02c2b06cf0603a37e8b3c896f93fe141f965529d
alex <enna@mailbox.org>
2019-03-20 12:31:06 +0100

    Initialize new Javascript

--- src/main/scala/com/eternitas/lastwill/LastWillStartup.scala
+++ src/main/scala/com/eternitas/lastwill/LastWillStartup.scala
@@ -1,9 +1,9 @@
 package com.lyrx.eternitas.lastwill
 
 import com.eternitas.wizard.{Bookmark, ContentSeqImpl, JQueryWrapper, WizardImpl}
-import org.querki.jquery.JQueryXHR
+import org.querki.jquery.{JQueryEventObject, JQueryXHR}
 import org.scalajs.dom.{XMLHttpRequest, document}
-import org.scalajs.dom.raw.Event
+import org.scalajs.dom.raw.{Element, Event}
 
 import scala.scalajs.js
 
@@ -14,10 +14,43 @@ object LastWillStartup {
   def main(args: Array[String]): Unit = {
     document.addEventListener(
       "DOMContentLoaded",
-      (e: Event) => {
-        println("Eternitas is initialized!")
-      }
+      (e: Event) => onStartup()
     )
   }
+
+
+
+  private def onStartup() = {
+    implicit val $ = initJQuery()
+
+    init()
+    println("Eternitas is initialized ")
+
+  }
+
+   def init()(implicit $:JQueryWrapper)={
+     $("#drop_zone").on("drop",(e:Element, evt:JQueryEventObject, t2:Any, t3:Any)=>{
+       evt.stopPropagation();
+       evt.preventDefault();
+       println("Dropped!!")
+
+     })
+
+     $("#drop_zone").on("dragover",(e:Element, evt:JQueryEventObject, t2:Any, t3:Any)=>{
+       evt.stopPropagation();
+       evt.preventDefault();
+       println("Dragged!!")
+
+     })
+
+
+   }
+
+  private def initJQuery():JQueryWrapper = {
+    js.Dynamic.global.jQuery
+      .noConflict()
+      .asInstanceOf[JQueryWrapper]
+  }
+
 
 }
