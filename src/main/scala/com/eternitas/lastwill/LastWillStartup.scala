1eb609038abc1aad5736da8c1f0926413a90f246
alex <enna@mailbox.org>
2019-03-22 18:05:45 +0100

    Encryption

--- src/main/scala/com/eternitas/lastwill/LastWillStartup.scala
+++ src/main/scala/com/eternitas/lastwill/LastWillStartup.scala
@@ -5,13 +5,16 @@ import org.querki.jquery.JQueryEventObject
 import org.scalajs.dom.document
 import org.scalajs.dom.raw._
 import com.eternitas.lastwill.DropDragHandler._
+import com.eternitas.lastwill.Eternitas
 import com.eternitas.lastwill.HashSum._
 
-import scala.scalajs.js ;
+import scala.concurrent.ExecutionContext
+import scala.scalajs.js;
+
+
 
 object LastWillStartup {
-
-
+  implicit val ec = ExecutionContext.global;
 
   def main(args: Array[String]): Unit = {
     document.addEventListener(
@@ -20,36 +23,44 @@ object LastWillStartup {
     )
   }
 
-
-
   private def onStartup() = {
     implicit val $ = initJQuery()
     init()
 
   }
 
-   def init()(implicit $:JQueryWrapper)={
+  def logo()(implicit $ : JQueryWrapper) =
+    $("#logo").click((e: Event) =>
+      new Eternitas().
+        withKeys().
+        onComplete(_.map(
+          _.keysOpt.
+            map(key=>{
+              println("Have key: " + key)
+            }))))
 
 
 
-     $("#drop_zone").
-       onDrop(
-         (file:File)=> new FileReader().
-           onHash(file,aHash=> {
-             $("#drop_zone").
-               removeClass("drop").
-               addClass("dropped").
-               html(aHash)
-           })
-       ).onDragOverNothing()
-   }
+  def init()(implicit $ : JQueryWrapper) = {
+    logo()
+    dropZone()
+  }
+
+  def dropZone()(implicit $ : JQueryWrapper) = $("#drop_zone").
+    onDrop(
+        (file: File) =>
+          new FileReader().onHash(file, aHash => {
+            $("#drop_zone").removeClass("drop").
+              addClass("dropped").
+              html(aHash)
+          })
+      ).onDragOverNothing()
 
 
-  private def initJQuery():JQueryWrapper = {
+  private def initJQuery(): JQueryWrapper = {
     js.Dynamic.global.jQuery
       .noConflict()
       .asInstanceOf[JQueryWrapper]
   }
-
 
 }
