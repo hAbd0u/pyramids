1406916c549aa1beb36af8054eb039a724574629
alex <enna@mailbox.org>
2019-03-22 17:29:22 +0100

    Encryption (begin)

--- src/main/scala/com/eternitas/lastwill/Encrypt.scala
+++ src/main/scala/com/eternitas/lastwill/Encrypt.scala
@@ -3,32 +3,44 @@ package com.eternitas.lastwill
 
 
 import org.scalajs.dom
-import dom.crypto.{HashAlgorithm, KeyUsage, RsaHashedKeyAlgorithm, crypto}
+import dom.crypto.{CryptoKey, HashAlgorithm, KeyUsage, RsaHashedKeyAlgorithm, crypto}
 
+import scala.concurrent.{ExecutionContext, Future}
 import scala.scalajs.js
 import scala.scalajs.js.typedarray.Uint8Array
 
 
 
-case class Eternitas(name:String) {
+case class Eternitas(keysOpt:Option[CryptoKey] =None) {
+
+
+
+  def withKeys()(implicit ctx:ExecutionContext) = {
+    if(keysOpt.isEmpty) Encrypt.generateKeys().map(key=>this.copy(keysOpt=Some(key)))
+    else
+      Future.successful(this.copy(keysOpt=Some(keysOpt.get)))
+
+  }
 
 
 
 
-def withKeys() = {
-
-  crypto.subtle.generateKey(
-    RsaHashedKeyAlgorithm.
-      `RSA-OAEP`(4096,
-        new Uint8Array( js.Array(1,0,1)),
-          HashAlgorithm.`SHA-256`
-        ),true,
-    js.Array(KeyUsage.encrypt,KeyUsage.decrypt))
-
-}}
+}
 
 
 object Encrypt {
+
+
+
+  def generateKeys()(implicit ctx:ExecutionContext):Future[CryptoKey]
+  = crypto.subtle.generateKey(
+    RsaHashedKeyAlgorithm.
+      `RSA-OAEP`(4096,
+        new Uint8Array( js.Array(1,0,1)),
+        HashAlgorithm.`SHA-256`
+      ),true,
+    js.Array(KeyUsage.encrypt,KeyUsage.decrypt)).
+    toFuture.map(_.asInstanceOf[CryptoKey])
 
 
 
