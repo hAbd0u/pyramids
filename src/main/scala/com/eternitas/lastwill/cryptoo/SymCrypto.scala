8a28ddf4005f5eeb32b4ec55a92263491acf112e
alex <enna@mailbox.org>
2019-03-27 17:44:48 +0100

    layout, start der seite

--- src/main/scala/com/eternitas/lastwill/cryptoo/SymCrypto.scala
+++ src/main/scala/com/eternitas/lastwill/cryptoo/SymCrypto.scala
@@ -1,5 +1,37 @@
 package com.eternitas.lastwill.cryptoo
 
-object SymCrypto {
+import org.scalajs.dom.crypto.{AesGcmParams, Algorithm, CryptoKey, KeyAlgorithmIdentifier, KeyUsage, crypto}
+
+import scala.scalajs.js
+import js.Dynamic.{literal => l}
+import scala.concurrent.ExecutionContext
+
+object SymCrypto extends SymCryptoTrait {
+  override val keyAlgorithmIdentifier:KeyAlgorithmIdentifier =l(
+
+    "name" -> "AES-GCM",
+    "length" -> 256
+  ).asInstanceOf[KeyAlgorithmIdentifier]
+
+}
+
+
+
+trait SymCryptoTrait {
+
+
+  val encryptDecrypt= js.Array(
+    KeyUsage.encrypt,
+    KeyUsage.decrypt)
+
+  val keyAlgorithmIdentifier:KeyAlgorithmIdentifier
+
+  def generateKey()(implicit ctx:ExecutionContext)= crypto.
+    subtle.
+    generateKey(keyAlgorithmIdentifier,true, encryptDecrypt).
+    toFuture.
+    map(_.asInstanceOf[CryptoKey])
+
+
 
 }
