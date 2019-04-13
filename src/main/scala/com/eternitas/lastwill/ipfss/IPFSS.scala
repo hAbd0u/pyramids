c3c39445de81ac735379a53d474c4f9f744bd0df
alex <enna@mailbox.org>
2019-04-13 15:24:30 +0200

    Remove signature from eternitas data

--- src/main/scala/com/eternitas/lastwill/ipfss/IPFSS.scala
+++ src/main/scala/com/eternitas/lastwill/ipfss/IPFSS.scala
@@ -2,8 +2,8 @@ package com.eternitas.lastwill.ipfss
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSGlobal
-
 import scala.scalajs.js.Dynamic.{literal => l}
+import scala.scalajs.js.Math
 
 @js.native
 @JSGlobal
@@ -18,7 +18,7 @@ object IPFSS {
     (js.Dynamic.newInstance(tag.constructor)(args)).asInstanceOf[C]
 
   def instance()={
-    instantiate[Ipfs](l())
+    instantiate[Ipfs](l("repo"  -> s"ipfs-${Math.random().toString()}" ))
    // js.Dynamic.newInstance(js.constructorOf[IPFS].constructor)().asInstanceOf[IPFS]
   }
 
