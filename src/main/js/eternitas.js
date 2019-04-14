020668724678596d71cf5665e71e5f5c0528115e
alex <enna@mailbox.org>
2019-04-14 18:53:21 +0200

    re-add eternitas.js

---
+++ src/main/js/eternitas.js
@@ -0,0 +1,17 @@
+
+
+function lyrx(){
+    var $ = jQuery.noConflict();
+
+    function loadFirst(){
+       // const node = new Ipfs({ repo: 'ipfs-' + Math.random() })
+    }
+
+
+    loadFirst();
+}
+
+
+document.addEventListener("DOMContentLoaded", lyrx);
+
+
