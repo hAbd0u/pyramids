cfacf5c1d4cc0f2d61dfe92a93eabbcd78ef6679
alex <enna@mailbox.org>
2019-04-16 21:33:22 +0200

    Remove unneeded files

--- snippets.sh
+++
@@ -1,9 +0,0 @@
-dir=src/main/resources/snippets
-CERT=~/Desktop/eternitas/eternitaskeys1.pem
-SCPTARGET=bitnami@34.204.6.252:/home/bitnami/apps/wordpress/htdocs/snippets
-
-for snippet in $(ls  $dir/*-frag.html)
-do
-scp -i $CERT  $snippet  $SCPTARGET
-done
-
