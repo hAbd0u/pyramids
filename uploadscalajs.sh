b0366c4631d091b7b297bc1020a9679981c5279b
alex <enna@mailbox.org>
2019-05-04 11:45:43 +0200

    make scala-js use webapp dir as output

--- uploadscalajs.sh
+++ uploadscalajs.sh
@@ -1,15 +1,2 @@
-TARGET=target/scala-2.12
-WEBAPP=src/main/webapp
-declare -a SCRIPTS=(
-    "eternitas-wordpress-opt.js"
-    "eternitas-wordpress-opt.js.map"
-    "eternitas-wordpress-jsdeps.js")
-
-
 source ~/env.sh
 sbt fullOptJS
-for script in "${SCRIPTS[@]}" 
-do
-cp   $TARGET/$script  $WEBAPP/js
-done
-#cp src/main/js/eternitas.js  $WEBAPP/js
