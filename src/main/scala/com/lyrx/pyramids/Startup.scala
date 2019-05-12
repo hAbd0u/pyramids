cdf83403f0679e7242bbc3aad696f4d1680e9d44
alex <enna@mailbox.org>
2019-05-12 14:56:11 +0200

    Refactoring, load javascript in a much better way

--- src/main/scala/com/lyrx/pyramids/Startup.scala
+++ src/main/scala/com/lyrx/pyramids/Startup.scala
@@ -1,10 +1,17 @@
 package com.lyrx.pyramids
 
 
+
+import org.scalajs.jquery._
+
+
 object Startup {
 
 
   def main(args: Array[String]): Unit ={
+    val $ = jQuery
+
+    println("JQuery"+$("sdf"))
 
   }
 
