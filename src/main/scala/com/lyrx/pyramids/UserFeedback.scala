c52a3055a5d06bbe80ad59dedecb3a4c7e54df4b
alex <enna@mailbox.org>
2019-05-14 16:02:46 +0200

    UserFeedBack, TODO foir Deshbandu

---
+++ src/main/scala/com/lyrx/pyramids/UserFeedback.scala
@@ -0,0 +1,15 @@
+package com.lyrx.pyramids
+
+
+
+import scala.concurrent.ExecutionContext
+import scala.scalajs.js.typedarray.ArrayBuffer
+
+trait UserFeedback {
+  def showTime(): UserFeedback
+  def message(s:String)
+  def error(s:String)
+  def log(msg:String,b:ArrayBuffer)(implicit exc:ExecutionContext)
+  def logString(msg:String)
+
+}
