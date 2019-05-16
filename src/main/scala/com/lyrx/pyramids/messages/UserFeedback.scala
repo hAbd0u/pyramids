63f8c972f39827d3ffe808537d2e8a3a8eeb3a12
Deshbandhu Mishra <deshbandhu.mishra@gmail.com>
2019-05-16 13:59:33 +0530

    Merge branch 'deshbandu' of https://github.com/lyrx/pyramids into deshbandhu
    # Conflicts:
    #	src/main/scala/com/lyrx/pyramids/Startup.scala

--- src/main/scala/com/lyrx/pyramids/messages/UserFeedback.scala
+++ src/main/scala/com/lyrx/pyramids/messages/UserFeedback.scala
@@ -3,11 +3,11 @@ package com.lyrx.pyramids.messages
 import scala.concurrent.ExecutionContext
 import scala.scalajs.js.typedarray.ArrayBuffer
 
-trait UserFeedback {
-  def showTime(): UserFeedback
-  def message(s:String)
-  def error(s:String)
-  def log(msg:String,b:ArrayBuffer)(implicit exc:ExecutionContext)
-  def logString(msg:String)
-
-}
+//trait UserFeedback {
+//  def showTime(): UserFeedback
+//  def message(s:String)
+//  def error(s:String)
+//  def log(msg:String,b:ArrayBuffer)(implicit exc:ExecutionContext)
+//  def logString(msg:String)
+//
+//}
