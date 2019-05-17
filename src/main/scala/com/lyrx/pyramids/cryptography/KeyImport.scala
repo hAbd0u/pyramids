f6f8d35d73221a430b7f62472a6acf93cedf5eed
Deshbandhu Mishra <deshbandhu.mishra@gmail.com>
2019-05-17 19:28:53 +0530

    Merge branch 'deshbandu' of https://github.com/lyrx/pyramids into deshbandu
    # Conflicts:
    #	src/main/scala/com/lyrx/pyramids/messages/Upload.scala

--- src/main/scala/com/lyrx/pyramids/cryptography/KeyImport.scala
+++ src/main/scala/com/lyrx/pyramids/cryptography/KeyImport.scala
@@ -14,7 +14,10 @@ trait KeyImport extends  SymetricCrypto with AsymetricCrypto {
     map(webKey => importSymetricKey(webKey).
       map(k=>Some(k))).getOrElse(Future{None}).
     map(ko=> new Pyramid(pyramidConfig.copy(symKeyOpt = ko)))
-
+/*
+class KeyImport: function importAsymKey, do the same for the signature keys: "importSignKey"
+It is the same as for the asymetric key. Try!
+* */
 
   def importAsymKey(walletNative: WalletNative)(implicit  executionContext: ExecutionContext) = walletNative.
     asym.
@@ -27,6 +30,15 @@ trait KeyImport extends  SymetricCrypto with AsymetricCrypto {
     getOrElse(Future{None}).
     map(e=>new Pyramid(pyramidConfig.copy(asymKeyOpt = e)))
 
-
+  def importSignKey(walletNative: WalletNative)(implicit  executionContext: ExecutionContext) = walletNative.
+    asym.
+    map(kp=>
+      importKeyPair(
+        keyPairNative = kp,
+        privateUsage = usageDecrypt,
+        publicUsage = usageEncrypt).
+        map(e=>Some(e))).
+    getOrElse(Future{None}).
+    map(e=>new Pyramid(pyramidConfig.copy(signKeyOpt = e)))
 
 }
