d546f6786194d21ac248671faba7d26caa105589
alex <enna@mailbox.org>
2019-05-22 20:10:16 +0200

    Remove erroneous functions

--- src/main/scala/com/lyrx/pyramids/cryptography/Signing.scala
+++ src/main/scala/com/lyrx/pyramids/cryptography/Signing.scala
@@ -1,6 +1,7 @@
 package com.lyrx.pyramids.cryptography
 
 import com.lyrx.pyramids.{DistributedData, Pyramid, PyramidConfig}
+import org.scalajs.dom.crypto.CryptoKeyPair
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.scalajs.js.typedarray.ArrayBuffer
@@ -10,5 +11,10 @@ trait Signing extends  AsymetricCrypto  {
     val pyramidConfig: PyramidConfig
 
 
+
+    def signArrayBuffer(data:ArrayBuffer)
+            (implicit executionContext: ExecutionContext) = pyramidConfig.
+      signKeyOpt.map(key=>sign(key,data).map(Some(_))).
+      getOrElse(Future{None})
 
 }
