79cf1f27857e52b04d46e2e089d91bb08f3fec84
alex <enna@mailbox.org>
2019-05-22 15:43:31 +0200

    Zipping

--- src/main/scala/com/lyrx/pyramids/demo/Startup.scala
+++ src/main/scala/com/lyrx/pyramids/demo/Startup.scala
@@ -2,9 +2,11 @@ package com.lyrx.pyramids.demo
 
 import com.lyrx.pyramids.cryptography.DragAndDrop
 import com.lyrx.pyramids.frontend.UserFeedback
+import com.lyrx.pyramids.jszip.JSZip
 import com.lyrx.pyramids.{Pyramid, PyramidConfig}
 import org.scalajs.dom.{Event, document}
 import org.scalajs.jquery.{JQuery, JQueryEventObject, jQuery => $}
+
 import scala.concurrent.{ExecutionContext, Future}
 
 object Startup extends DragAndDrop with UserFeedback{
@@ -34,6 +36,8 @@ object Startup extends DragAndDrop with UserFeedback{
 // Newly added code by Alex
   def init(pyramidConfig: PyramidConfig)(
       implicit executionContext: ExecutionContext): PyramidConfig = {
+
+    println("jSZip: " + new JSZip())
 
     val pyramid = new Pyramid(pyramidConfig)
     def handle(f: Future[PyramidConfig]) = f.map(config => init(config))
---
+++ src/main/scala/com/lyrx/pyramids/jszip/JSZip.scala
@@ -0,0 +1,19 @@
+package com.lyrx.pyramids.jszip
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.{JSGlobal, JSGlobalScope}
+
+
+
+@JSGlobal
+@js.native
+class JSZip extends js.Object{
+
+
+  def test():String = js.native
+}
+
+
+trait Zipping{
+
+}
