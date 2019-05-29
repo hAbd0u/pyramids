330db4ab7ab5808d62311622673bb60df5270a2b
alex <enna@mailbox.org>
2019-05-29 10:58:24 +0200

    Refactor pyramidconfig: Remove DistributedDir

--- src/main/scala/com/lyrx/pyramids/jszip/JSZip.scala
+++ src/main/scala/com/lyrx/pyramids/jszip/JSZip.scala
@@ -1,34 +1,19 @@
 package com.lyrx.pyramids.jszip
 
 
-import org.scalajs.dom.raw.Blob
 
-import scala.scalajs.js
-import scala.scalajs.js.{Promise, |}
-import scala.scalajs.js.annotation.{JSGlobal, JSGlobalScope}
-import scala.scalajs.js.typedarray.{ArrayBuffer, Uint8Array}
-
-
-
-@JSGlobal
-@js.native
-class JSZip extends js.Object{
-
-
-  def file(name:String,data:JSData):String = js.native
-}
-
-@JSGlobal()
-@js.native
-object JSZip extends js.Object{
-
-//  def loadAsync(b:ArrayBuffer):Promise[Unit] = js.native
-}
-
+import typings.jszipLib.jszipMod.{ ^ => JJSZip}
 
 
 
 trait Zipping{
+
+
+  def zip()={
+
+    JJSZip.file("")
+
+  }
 
 
 
