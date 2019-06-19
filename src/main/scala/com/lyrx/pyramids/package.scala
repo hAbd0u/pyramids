3ba6ec07df6351e5dddcc6b68d1e352665add3dc
alex <enna@mailbox.org>
2019-06-19 17:58:51 +0200

    FutureOption

--- src/main/scala/com/lyrx/pyramids/package.scala
+++ src/main/scala/com/lyrx/pyramids/package.scala
@@ -3,6 +3,7 @@ package com.lyrx
 import org.scalajs.dom.raw.{Blob, File}
 import typings.nodeLib.bufferMod.Buffer
 
+import scala.concurrent.{ExecutionContext, Future}
 import scala.scalajs.js
 import scala.scalajs.js.typedarray.ArrayBuffer
 import scala.scalajs.js.|
@@ -11,6 +12,18 @@ package object pyramids {
 
   type TextFieldContents =
     js.UndefOr[java.lang.String | scala.Double | js.Array[java.lang.String]]
+
+
+  class FutureOption[T](val m:Future[Option[T]]){
+
+    def fmap[U](tf:T=>U)(implicit executionContext: ExecutionContext) = m.map(_.map(tf(_)))
+
+
+    def fflatMap[U](tf:T=>FutureOption[U])
+                   (implicit executionContext: ExecutionContext) :FutureOption[U]= new FutureOption[U](this.m.
+      map(_.
+        map(tf(_)).getOrElse(new FutureOption[U](Future{None}))).map(_.m).flatten)
+  }
 
 
 
