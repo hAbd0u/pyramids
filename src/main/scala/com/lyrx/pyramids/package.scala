9bff89dff0ee1bde70dceaaba2d4a76aa0725fee
alex <enna@mailbox.org>
2019-06-19 18:05:18 +0200

    FutureOption

--- src/main/scala/com/lyrx/pyramids/package.scala
+++ src/main/scala/com/lyrx/pyramids/package.scala
@@ -1,11 +1,7 @@
 package com.lyrx
 
-import org.scalajs.dom.raw.{Blob, File}
-import typings.nodeLib.bufferMod.Buffer
-
 import scala.concurrent.{ExecutionContext, Future}
 import scala.scalajs.js
-import scala.scalajs.js.typedarray.ArrayBuffer
 import scala.scalajs.js.|
 
 package object pyramids {
@@ -13,18 +9,19 @@ package object pyramids {
   type TextFieldContents =
     js.UndefOr[java.lang.String | scala.Double | js.Array[java.lang.String]]
 
+  implicit class FutureOption[T](val m: Future[Option[T]]) {
 
-  implicit class FutureOption[T](val m:Future[Option[T]]){
+    def fmap[U](tf: T => U)(
+        implicit executionContext: ExecutionContext): FutureOption[U] =
+      new FutureOption[U](m.map(_.map(tf(_))))
 
-    def fmap[U](tf:T=>U)(implicit executionContext: ExecutionContext):FutureOption[U] = new FutureOption[U](m.map(_.map(tf(_))))
-
-
-    def fflatMap[U](tf:T=>FutureOption[U])
-                   (implicit executionContext: ExecutionContext) :FutureOption[U]= new FutureOption[U](this.m.
-      map(_.
-        map(tf(_)).getOrElse(new FutureOption[U](Future{None}))).map(_.m).flatten)
+    def fflatMap[U](tf: T => FutureOption[U])(
+        implicit executionContext: ExecutionContext): FutureOption[U] =
+      new FutureOption[U](
+        this.m
+          .map(_.map(tf(_)).getOrElse(new FutureOption[U](Future { None })))
+          .map(_.m)
+          .flatten)
   }
-
-
 
 }
