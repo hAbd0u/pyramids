81b3159c65c79573ce1677f4e2d6e49bad2b0520
alex <enna@mailbox.org>
2019-06-29 20:08:08 +0200

    Examples

--- src/main/scala/com/lyrx/pyramids/demo/NinetyNineProblems.scala
+++ src/main/scala/com/lyrx/pyramids/demo/NinetyNineProblems.scala
@@ -6,7 +6,7 @@ trait NinetyNineProblems {
 
 
 
-  implicit class PimpedListList[T](ll:ListList[T]) {
+   class PimpedListList[T](ll:ListList[T]) {
 
     def collect(b:T)(implicit comp:Equiv[T]) = ll match {
       case (aHead:List[T]) :: (aTail:ListList[T]) => aHead  match {
@@ -23,16 +23,7 @@ trait NinetyNineProblems {
 
 
   def pack[T](l:List[T]) (implicit comp:Equiv[T])= l.foldLeft(
-    Nil:ListList[T]) (( (ll:ListList[T]),(b:T)) => ll match {
-    case (aHead:List[T]) :: (aTail:ListList[T]) => aHead  match {
-      case h  :: t => if(comp.equiv(h,b))
-        (b +: aHead) +: aTail//same element again
-      else
-        List(b) +: ll   // new element, not the same as the one before
-      case Nil => List(b) +: ll // new element
-    }
-    case Nil => List(List(b)) //first element ever
-  }).reverse
+    Nil:ListList[T]) ( (ll,b) => (new PimpedListList(ll)).collect(b)).reverse
 
   def testPack()={
     println(pack(list1))
