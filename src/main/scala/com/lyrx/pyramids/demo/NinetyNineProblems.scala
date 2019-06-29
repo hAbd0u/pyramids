7a0b79cf7c6dc078210b76b8855e2be044bf843d
alex <enna@mailbox.org>
2019-06-29 21:43:58 +0200

    Examples

--- src/main/scala/com/lyrx/pyramids/demo/NinetyNineProblems.scala
+++ src/main/scala/com/lyrx/pyramids/demo/NinetyNineProblems.scala
@@ -8,14 +8,19 @@ trait NinetyNineProblems {
 
    class PimpedListList[T](ll:ListList[T]) {
 
+     def matchList(b:T, aHead:List[T] , aTail:ListList[T])
+                  (implicit comp:Equiv[T]) = aHead  match {
+       case h  :: t => if(comp.equiv(h,b))
+         (b +: aHead) +: aTail//same element again
+       else
+         List(b) +: ll   // new element, not the same as the one before
+       case Nil => List(b) +: ll // new element
+     }
+
+
+
     def collect(b:T)(implicit comp:Equiv[T]) = ll match {
-      case (aHead:List[T]) :: (aTail:ListList[T]) => aHead  match {
-        case h  :: t => if(comp.equiv(h,b))
-          (b +: aHead) +: aTail//same element again
-        else
-          List(b) +: ll   // new element, not the same as the one before
-        case Nil => List(b) +: ll // new element
-      }
+      case (aHead:List[T]) :: (aTail:ListList[T]) => matchList(b,aHead,aTail)
       case Nil => List(List(b)) //first element ever
     }
 
@@ -25,8 +30,12 @@ trait NinetyNineProblems {
   def pack[T](l:List[T]) (implicit comp:Equiv[T])= l.foldLeft(
     Nil:ListList[T]) ( (ll,b) => (new PimpedListList(ll)).collect(b)).reverse
 
+  def  compress[T](l:List[T]) (implicit comp:Equiv[T])= pack(l).
+    map(l=>(l.length,l.head))
+
   def testPack()={
     println(pack(list1))
+    println(compress(list1))
   }
 
 
