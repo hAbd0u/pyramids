c8ec96e26ebfa80ee060df02dc214c8dfa558089
alex <enna@mailbox.org>
2019-06-30 11:18:56 +0200

    Refactoring

--- src/main/scala/com/lyrx/pyramids/ipfs/CanIpfs.scala
+++ src/main/scala/com/lyrx/pyramids/ipfs/CanIpfs.scala
@@ -1,8 +1,13 @@
 package com.lyrx.pyramids.ipfs
 
-
 import com.lyrx.pyramids.pcrypto.{AsymetricCrypto, WalletNative}
-import com.lyrx.pyramids.{InfuraIpfsImpl, Pyramid, PyramidConfig, PyramidJSON, pcrypto}
+import com.lyrx.pyramids.{
+  InfuraIpfsImpl,
+  Pyramid,
+  PyramidConfig,
+  PyramidJSON,
+  pcrypto
+}
 import org.scalajs.dom.crypto.CryptoKey
 import typings.{nodeLib, stdLib}
 import typings.nodeLib.bufferMod
@@ -15,15 +20,13 @@ import scala.scalajs.js.typedarray.ArrayBuffer
 trait CanIpfs extends pcrypto.Crypto with PyramidJSON with AsymetricCrypto {
   val pyramidConfig: PyramidConfig
 
-  def initIpfs()(implicit executionContext: ExecutionContext):Future[Pyramid]
-
-
+  def initIpfs()(implicit executionContext: ExecutionContext): Future[Pyramid]
 
   def initIpfsAndPublishPublicKeys()(
-      implicit executionContext: ExecutionContext) :Future[Pyramid]
+      implicit executionContext: ExecutionContext): Future[Pyramid]
 
-  def exportAllPublicKeys()(
-      implicit ctx: ExecutionContext): Future[pcrypto.CryptoTypes.JsonWebKeyOptPair] =
+  def exportAllPublicKeys()(implicit ctx: ExecutionContext)
+    : Future[pcrypto.CryptoTypes.JsonWebKeyOptPair] =
     exportPublicKey(pyramidConfig.asymKeyOpt).flatMap(pubKeyOpt =>
       exportPublicKey(pyramidConfig.signKeyOpt).map(signKeyOpt =>
         (pubKeyOpt, signKeyOpt)))
@@ -49,99 +52,84 @@ trait CanIpfs extends pcrypto.Crypto with PyramidJSON with AsymetricCrypto {
             .getOrElse(Future { None }))
       .map(_.flatMap(_.headOption.map(_.hash)))
       .map(_.map(s =>
-        new Pyramid(pyramidConfig.
-          withPubKeys(s).
-          msg(s"Oh ${pyramidConfig.name()}, we have published your divine signature!")))
+        new Pyramid(pyramidConfig
+          .withPubKeys(s)
+          .msg(s"Oh ${pyramidConfig.name()}, we have published your divine signature!")))
         .getOrElse(new Pyramid(pyramidConfig)))
 
-  def saveBufferToIpfs(f:Future[nodeLib.Buffer])(implicit ctx: ExecutionContext) =
-    f.flatMap(b=>pyramidConfig.infuraClientOpt
-      .map(ipfs => ipfs.futureAdd(b).map(Some(_))).getOrElse(Future{None}))
+  def saveBufferToIpfs(f: Future[nodeLib.Buffer])(
+      implicit ctx: ExecutionContext) =
+    f.flatMap(
+      b =>
+        pyramidConfig.infuraClientOpt
+          .map(ipfs => ipfs.futureAdd(b).map(Some(_)))
+          .getOrElse(Future { None }))
 
-  def saveArrayBufferToIpfs(f:Future[ArrayBuffer])(implicit ctx: ExecutionContext) =
-    saveBufferToIpfs(f.map(b=>bufferMod.Buffer.from(b)))
+  def saveArrayBufferToIpfs(f: Future[ArrayBuffer])(
+      implicit ctx: ExecutionContext) =
+    saveBufferToIpfs(f.map(b => bufferMod.Buffer.from(b)))
 
+  def saveStringToIpfs(s: String)(implicit ctx: ExecutionContext) =
+    saveBufferToIpfs(
+      Future { bufferMod.Buffer.from(s) }
+    )
 
-
-
-  def saveStringToIpfs(s:String)(implicit ctx: ExecutionContext) =saveBufferToIpfs(
-    Future{bufferMod.Buffer.from(s)}
-  )
-
-  def savePubKeyEncryptedStringToIpfs(s:String) (implicit ctx: ExecutionContext)= {
+  def savePubKeyEncryptedStringToIpfs(s: String)(
+      implicit ctx: ExecutionContext) = {
     //println(s"Saving '${s}'")
-    pyramidConfig.
-      asymKeyOpt.map(k=>saveArrayBufferToIpfs(asymEncryptString(k.publicKey,s))).getOrElse(Future{None})
+    pyramidConfig.asymKeyOpt
+      .map(k => saveArrayBufferToIpfs(asymEncryptString(k.publicKey, s)))
+      .getOrElse(Future { None })
   }
-
-
-
-
 
   def bufferToIpfs(buffer: nodeLib.Buffer)(implicit ctx: ExecutionContext) =
     pyramidConfig.infuraClientOpt
       .map(_.futureAdd(buffer).map(l => Some(l.head.hash)))
       .getOrElse(Future { None })
 
-  def readIpfs(aHash:String)
-              (implicit executionContext: ExecutionContext)
-  = pyramidConfig.
-    infuraClientOpt.map(ipfsClient => ipfsClient.
-    futureCat(aHash).map(Some(_))).
-    getOrElse(Future{None})
+  def readIpfs(aHash: String)(implicit executionContext: ExecutionContext) =
+    pyramidConfig.infuraClientOpt
+      .map(ipfsClient => ipfsClient.futureCat(aHash).map(Some(_)))
+      .getOrElse(Future { None })
 
+  def readIpfsString(aHash: String)(
+      implicit executionContext: ExecutionContext) =
+    pyramidConfig.infuraClientOpt
+      .map(ipfsClient => ipfsClient.futureCatString(aHash).map(Some(_)))
+      .getOrElse(Future { None })
 
+  def readAndDecrypt(aHash: String)(
+      implicit executionContext: ExecutionContext) =
+    pyramidConfig.infuraClientOpt
+      .map(
+        ipfsClient =>
+          ipfsClient
+            .futureCat(aHash)
+            .flatMap(((b: nodeLib.Buffer) => {
+              pyramidConfig.asymKeyOpt
+                .map(k =>
+                  asymDecryptBuffer(k.privateKey, b).map(b =>
+                    Some(b.myToString())))
+                .getOrElse(Future { None })
+            })))
+      .getOrElse(Future { None })
 
+  def readIpfsNativeWallet()(implicit executionContext: ExecutionContext) =
+    readIpfsString(pyramidConfig.ipfsData.pharaoData.pubkey).map(_.map(s =>
+      JSON.parse(s).asInstanceOf[WalletNative]))
 
+  def readPharaoKeys()(implicit executionContext: ExecutionContext) =
+    readIpfsNativeWallet().flatMap(
+      _.flatMap(nativeWallet =>
+        nativeWallet.`asym`.toOption.flatMap(_.`public`.toOption.map(k =>
+          importKey(jsonWebKey = k, usages = usageEncrypt, aHashAlgorithm))))
+        .getOrElse(Future { None }))
 
-  def readIpfsString(aHash:String)
-              (implicit executionContext: ExecutionContext)
-  =   pyramidConfig.
-    infuraClientOpt.map(ipfsClient => ipfsClient.
-    futureCatString(aHash).map(Some(_))).
-    getOrElse(Future{None})
-
-  def readAndDecrypt(aHash:String)(implicit executionContext: ExecutionContext)=  pyramidConfig.
-    infuraClientOpt.map(ipfsClient => ipfsClient.
-    futureCat(aHash).flatMap(((b:nodeLib.Buffer)=>{
-    pyramidConfig.
-      asymKeyOpt.
-      map(k=>
-        asymDecryptBuffer(
-          k.privateKey,b).
-          map(b=>Some(b.myToString()))).
-      getOrElse(Future{None})
-  }))).getOrElse(Future{None})
-
-
-
-
-  def readIpfsNativeWallet()
-                    (implicit executionContext: ExecutionContext)
-  = readIpfsString(pyramidConfig.ipfsData.pharaoData.pubkey).
-    map(_.map(s=>JSON.parse(s).asInstanceOf[WalletNative]))
-
-
-
-  def readPharaoKeys()
-                      (implicit executionContext: ExecutionContext)
-  = readIpfsNativeWallet().flatMap(_.flatMap(nativeWallet=>
-    nativeWallet.`asym`.toOption.flatMap(_.`public`.toOption.map(k =>
-      importKey(
-        jsonWebKey = k ,
-        usages = usageEncrypt,
-        aHashAlgorithm)
-    ))).getOrElse(Future{None}))
-
-
-
-  def readPharaoWallet()
-                    (implicit executionContext: ExecutionContext)
-  = readPharaoKeys().map( (keyOpt:Option[CryptoKey])=>
-    keyOpt.map(key=>pyramidConfig.msg(
-      s"Oh ${pyramidConfig.name()}, here are your public keys!")))
-
-
-
+  def readPharaoWallet()(implicit executionContext: ExecutionContext) =
+    readPharaoKeys().map(
+      (keyOpt: Option[CryptoKey]) =>
+        keyOpt.map(key =>
+          pyramidConfig.msg(
+            s"Oh ${pyramidConfig.name()}, here are your public keys!")))
 
 }
--- src/main/scala/com/lyrx/pyramids/ipfs/package.scala
+++ src/main/scala/com/lyrx/pyramids/ipfs/package.scala
@@ -1,6 +1,5 @@
 package com.lyrx.pyramids
 
-
 import typings.{nodeLib, stdLib}
 import typings.stdLib.{ArrayBuffer, Uint8Array}
 
@@ -13,79 +12,59 @@ import typings.nodeLib.bufferMod
 
 import scala.language.implicitConversions
 
-
-
 package object ipfs {
-
 
   type CanBuffer = String | ArrayBuffer | Uint8Array
 
-
   type OrError = js.UndefOr[js.Error]
   type ErrorPromise = js.Promise[OrError]
-  type ErrorCallback = js.Function1[OrError,Unit]
+  type ErrorCallback = js.Function1[OrError, Unit]
 
-  type PubSubHandler = js.Function1[PubSubMessage,Unit]
+  type PubSubHandler = js.Function1[PubSubMessage, Unit]
+
+  implicit def typedArrayArrayBufferToStdlib(
+      b: js.typedarray.ArrayBuffer): stdLib.ArrayBuffer =
+    b.asInstanceOf[stdLib.ArrayBuffer]
+  implicit def jstdLibArrayBufferToTypedArrayArrayBuffer(
+      b: js.typedarray.ArrayBuffer): js.typedarray.ArrayBuffer =
+    b.asInstanceOf[js.typedarray.ArrayBuffer]
 
 
-  implicit def typedArrayArrayBufferToStdlib(b:js.typedarray.ArrayBuffer):stdLib.ArrayBuffer = b.asInstanceOf[stdLib.ArrayBuffer]
-  implicit def jstdLibArrayBufferToTypedArrayArrayBuffer(b:js.typedarray.ArrayBuffer):js.typedarray.ArrayBuffer = b.asInstanceOf[js.typedarray.ArrayBuffer]
+  implicit class PimpedString(b: js.typedarray.ArrayBuffer) {
 
-  trait PubSubSupport{
-    val ipfsClient:IpfsClient
-
-    def pubsubPublish(topic:String,message:String): Future[OrError] = ipfsClient.pubsub.publish(topic,bufferMod.Buffer.from(message)).toFuture
-
-    def pubsubSubscribe(topic:String,h:PubSubHandler): Future[OrError] = ipfsClient.pubsub.subscribe(topic,h).toFuture
-
-    def pubsubUnsubscribe(topic:String,h:PubSubHandler): Future[OrError] = ipfsClient.pubsub.unsubscribe(topic,h).toFuture
-
-    def pubsubLs(): Future[js.Array[String]] = ipfsClient.pubsub.ls().toFuture
-
-    def pubsubPeers(topic:String): Future[js.Array[String]] =ipfsClient.pubsub.peers(topic).toFuture
+    def myToString() = new TextDecoder().decode(new js.typedarray.Uint8Array(b))
   }
 
+  implicit class PimpedIpfsClient(val ipfsClient: IpfsClient)
+      extends PubSubSupport
+      with PinSupport {
 
-  trait PinSupport {
-    val ipfsClient: IpfsClient
+    def futureCat(s: String): Future[nodeLib.Buffer] =
+      ipfsClient.cat(s).toFuture
 
-    def pinAdd(h:String): Future[js.Array[PinResult]] = ipfsClient.pin.add(h).toFuture
-  }
+    def futureCatString(s: String) = ipfsClient.catString(s).toFuture
 
+    def futureAddString(s: String) = futureAdd(bufferMod.Buffer.from(s))
 
-  implicit class PimpedString(b:js.typedarray.ArrayBuffer){
+    def futurePin(s: String)(implicit executionContext: ExecutionContext) =
+      futureAddString(s).flatMap(
+        _.headOption
+          .map(r => pinAdd(r.hash).map(r2 => Some(r2)))
+          .getOrElse(Future { None }))
 
-    def myToString()= new TextDecoder().decode(new js.typedarray.Uint8Array(b))
-  }
-
-  implicit class PimpedIpfsClient(val ipfsClient:IpfsClient) extends  PubSubSupport  with PinSupport {
-
-
-    def futureCat(s:String):Future[nodeLib.Buffer] = ipfsClient.cat(s).toFuture
-
-    def futureCatString(s:String) = ipfsClient.catString(s).toFuture
-
-
-    def futureAddString(s:String) = futureAdd(bufferMod.Buffer.from(s))
-
-    def futurePin(s:String)(implicit executionContext: ExecutionContext) = futureAddString(s).
-      flatMap(_.headOption.map(r =>pinAdd(r.hash).map(r2=>Some(r2))).getOrElse(Future{None}))
-
-
-    def futureAdd(content:nodeLib.Buffer) = {
+    def futureAdd(content: nodeLib.Buffer) = {
       val promise = Promise[js.Array[IPFSSFile]]
 
-      ipfsClient.add(content,l(),(e,r)=>if(e != null )
-        promise.failure(new Throwable(e.toString))
-      else
-        promise.success(r)
-      )
+      ipfsClient.add(content,
+                     l(),
+                     (e, r) =>
+                       if (e != null)
+                         promise.failure(new Throwable(e.toString))
+                       else
+                         promise.success(r))
       promise.future
     }
 
   }
-
-
-
 
 }
